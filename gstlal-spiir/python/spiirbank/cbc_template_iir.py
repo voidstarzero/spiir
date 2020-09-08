1bc2324ffbf632f789a6f4c55443e9490ae51073
alex codoreanu <acodoreanu@swin.edu.au>
2020-09-08 12:30:28 +1000

    Formatted cbc_template_iir.py using yapf.

--- gstlal-spiir/python/spiirbank/cbc_template_iir.py
+++ gstlal-spiir/python/spiirbank/cbc_template_iir.py
@@ -1331,7 +1331,8 @@ class Bank(object):
                         b0dict.setdefault(sampleRate / M, []).append(
                             b0[i] * M**0.5 * a1[i]**(newdelay * M - delay[i]))
                         delaydict.setdefault(sampleRate / M, []).append(newdelay)
-
+                    #logging.info("sampleRate %4.0d, filter %3.0d, M %2.0d, f %10.9f, delay %d, newdelay %d" %
+                    # (sampleRate, i, M, f, delay[i], newdelay))
                 else:
                     a1dict[int(sampleRate)] = a1
                     b0dict[int(sampleRate)] = b0
